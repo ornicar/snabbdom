95c225d3d83e106d16aab7df78fa7f7babff2144
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-06-25 10:03:55 +0200

    drop unused attributes

--- src/modules/attributes.ts
+++ src/modules/attributes.ts
@@ -3,12 +3,8 @@ import {Module} from './module';
 
 export type Attrs = Record<string, string | number | boolean>
 
-const booleanAttrs = ["allowfullscreen", "async", "autofocus", "autoplay", "checked", "compact", "controls", "declare",
-                "default", "defaultchecked", "defaultmuted", "defaultselected", "defer", "disabled",
-                "enabled", "formnovalidate", "hidden", "indeterminate", "inert", "ismap", "itemscope", "loop", "multiple",
-                "muted", "nohref", "noresize", "noshade", "novalidate", "nowrap", "open", "pauseonexit", "readonly",
-                "required", "reversed", "scoped", "seamless", "selected", "sortable", "spellcheck", "translate",
-                "truespeed", "typemustmatch", "visible"];
+const booleanAttrs = ["autofocus", "checked", "disabled", "hidden", "readonly",
+                "required", "selected", "spellcheck", "translate"];
 
 const xlinkNS = 'http://www.w3.org/1999/xlink';
 const xmlNS = 'http://www.w3.org/XML/1998/namespace';
